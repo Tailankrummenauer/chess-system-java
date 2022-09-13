a6c68206a2c73ba4d25f2829f470b724cdf4e676
Tailankrummenauer <tailankrummenauer@gmail.com>
2022-09-13 17:27:41 -0300

    Starting to implement Board and Piece 3

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,20 @@
-package io.github.jiangdequan;
+package boardgame;
 
 public class Piece {
-    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null; // se nao colocar o java por padrao nulo
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+
+
+
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,20 +1,22 @@
+package boardgame;
 
 public class Position {
+    //encapsulamento dos campos
     private Integer row;
     private Integer column;
    
-   
+   //construtor
     public Position(Integer row, Integer column) {
         this.row = row;
         this.column = column;
     }
 
-
+//encapsulamento
     public Integer getRow() {
         return row;
     }
 
-
+//encapsulamento
     public void setRow(Integer row) {
         this.row = row;
     }
@@ -34,7 +36,10 @@ public class Position {
         this.column = column;
     }
     
-    @Override
+    //conceito object é uma super classe de todas classes
+    
+    @Override //sobreposição de metodo toString que é da classe object
+
     public String toString() {
         return "Position: " +
                 "row = " + row +

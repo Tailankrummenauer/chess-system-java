dfae17b01b293039caf131dcdd5ab8d21cdf27e7
Tailankrummenauer <tailankrummenauer@gmail.com>
2022-09-19 16:55:32 -0300

    implement possible moves of rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,47 +2,45 @@ package boardgame;
 
 public class Position {
     //encapsulamento dos campos
-    private Integer row;
-    private Integer column;
+    private int row;
+    private int column;
    
    //construtor
-    public Position(Integer row, Integer column) {
+    public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
 //encapsulamento
-    public Integer getRow() {
+    public int getRow() {
         return row;
     }
 
 //encapsulamento
-    public void setRow(Integer row) {
+    public void setRow(int row) {
         this.row = row;
     }
 
+    
 
-    public Integer getColumn() {
+    public int getColumn() {
         return column;
     }
 
 
-    public void setColumn(Integer column) {
+    public void setColumn(int column) {
         this.column = column;
     }
 
-    public void setValues (Integer row, Integer column){
-        this.row = row;
-        this.column = column;
-    }
-    
     //conceito object é uma super classe de todas classes
     
-    @Override //sobreposição de metodo toString que é da classe object
+    public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
-    public String toString() {
-        return "Position: " +
-                "row = " + row +
-                ", column = " + column;
-                }
+	@Override
+	public String toString() {
+		return row + ", " + column;
+    }
 }
